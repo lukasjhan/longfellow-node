87048a29b77e4b7994ccf16ca6ec4a85c0196dd8
Lukas J Han <lukas.j.han@gmail.com>
2025-09-05 10:24:11 +0900

    feat: edit include

--- src/addon.cpp
+++ src/addon.cpp
@@ -2,7 +2,7 @@
 #include <cstdint>
 
 // 퍼블릭 API 헤더(공식 가이드에서 제시)
-#include "/home/lukasjhan/longfellow-zk/install/include/mdoc_zk.h"
+#include "mdoc_zk.h"
 
 Napi::Value ping(const Napi::CallbackInfo& info) {
   return Napi::String::New(info.Env(), "longfellow-ok");
